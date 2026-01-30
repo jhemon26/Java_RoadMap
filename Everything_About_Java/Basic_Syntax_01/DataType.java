317d11dc286fea514b4d771fec3bb8d0e7802cfe
Jahid H Emon <je2264r@gre.ac.uk>
2026-01-30 21:50:20 +0000

    Uploded Basic Syntax, Data Type

---
+++ Everything_About_Java/Basic_Syntax_01/DataType.java
@@ -0,0 +1,42 @@
+
+// Let's see some data type in java and example of useges
+
+public class DataType {
+    public static void main(String[] args) {
+        int num1 = 5;           // int to store whole intiger value in variable
+        float num2 = 10.13f;    // float to store fractional values "add "f" at the last"
+        double num3 = 20.23d;   // double to store big fractional values "end with d"
+        var num4 = 40;          // Lastlly the "var" data type will catch the veriable status automatically
+
+
+        // lets perform some basic Arithmetic and see what happens
+        var additon = (num1 + num2);             
+        var subtraction = (num2 - num3);
+        var multiplication = (num3 * num4);
+        var division = (num4 / num1);
+        var modulo = (num3 % num1);
+
+
+        // Let's see the result 
+        System.out.println("the additionm of " + num1 + " and " + num2 + " is " + additon); 
+        System.out.println("Subtraction " + subtraction);
+        System.out.println("Multiplication " + multiplication);
+        System.out.println("Division " +  division);
+        System.out.println("Modulo " + modulo );
+
+
+
+        // We have assignment in java and we can perform increment and decrement like below
+        int fruits = 0;
+        var fruitscount = 10;
+        while (fruits < 10) {
+            fruits += 1;            // This is an example of asigning increment where fruits increases by 1
+            fruitscount--;          // Here -- means we are decreasing fruitscount by 1
+            System.out.println("Fruits in now " + fruits);
+            
+            
+        }
+        
+ }
+    
+}
---
+++ Everything_About_Java/Hello_World.java
@@ -0,0 +1,10 @@
+// Horrrahhhh! It's the start of your day one with JAVA 
+// Everything in java has to be inside a class like, 
+
+public class Hello_World {                              //Your class name should match filename
+    public static void main (String[] args)     // main method - start program execution 
+    {
+        System.out.println("Hello world");   // System is a class, out is an object, println is a method
+
+    }
+}
